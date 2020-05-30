8964cede683d89d957b644ee89e1a9b012a5a35d
Arne Lapõnin <arne.laponin@gmail.com>
2020-05-30 12:54:52 +0200

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import React, { ReactElement } from "react";
+import Layout from "../components/layout";
+
+function HomePage(): ReactElement {
+    return (
+        <Layout>
+            <h1>Arne Lapõnin's page is coming soon!</h1>
+        </Layout>
+    );
+}
+
+export default HomePage;
